35011ba09be9a61a53d406e6c702e34956ab9304
jameshackl <james.f.hackl@gmail.com>
2017-08-21 17:08:26 -0600

    started putting together the UI. Aimed at debugging right now.

--- JameBoyV1/MainWindow.Designer.cs
+++ JameBoyV1/MainWindow.Designer.cs
@@ -28,20 +28,260 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelRegisterA = new System.Windows.Forms.Label();
+            this.groupBoxRegisters = new System.Windows.Forms.GroupBox();
+            this.textBoxRegisterA = new System.Windows.Forms.TextBox();
+            this.textBoxRegisterB = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBoxRegisterD = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxRegisterC = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxRegisterF = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBoxRegisterE = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBoxRegisterL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxRegisterH = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonStartEmulation = new System.Windows.Forms.Button();
+            this.buttonPauseEmulation = new System.Windows.Forms.Button();
+            this.buttonStopEmulation = new System.Windows.Forms.Button();
+            this.textBoxMessages = new System.Windows.Forms.TextBox();
+            this.groupBoxRegisters.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelRegisterA
+            // 
+            this.labelRegisterA.AutoSize = true;
+            this.labelRegisterA.Location = new System.Drawing.Point(30, 32);
+            this.labelRegisterA.Name = "labelRegisterA";
+            this.labelRegisterA.Size = new System.Drawing.Size(17, 17);
+            this.labelRegisterA.TabIndex = 0;
+            this.labelRegisterA.Text = "A";
+            // 
+            // groupBoxRegisters
+            // 
+            this.groupBoxRegisters.Controls.Add(this.textBoxRegisterL);
+            this.groupBoxRegisters.Controls.Add(this.label2);
+            this.groupBoxRegisters.Controls.Add(this.textBoxRegisterH);
+            this.groupBoxRegisters.Controls.Add(this.label3);
+            this.groupBoxRegisters.Controls.Add(this.textBoxRegisterF);
+            this.groupBoxRegisters.Controls.Add(this.label11);
+            this.groupBoxRegisters.Controls.Add(this.textBoxRegisterE);
+            this.groupBoxRegisters.Controls.Add(this.label13);
+            this.groupBoxRegisters.Controls.Add(this.textBoxRegisterD);
+            this.groupBoxRegisters.Controls.Add(this.label1);
+            this.groupBoxRegisters.Controls.Add(this.textBoxRegisterC);
+            this.groupBoxRegisters.Controls.Add(this.label10);
+            this.groupBoxRegisters.Controls.Add(this.textBoxRegisterB);
+            this.groupBoxRegisters.Controls.Add(this.label12);
+            this.groupBoxRegisters.Controls.Add(this.textBoxRegisterA);
+            this.groupBoxRegisters.Controls.Add(this.labelRegisterA);
+            this.groupBoxRegisters.Location = new System.Drawing.Point(497, 35);
+            this.groupBoxRegisters.Name = "groupBoxRegisters";
+            this.groupBoxRegisters.Size = new System.Drawing.Size(148, 257);
+            this.groupBoxRegisters.TabIndex = 9;
+            this.groupBoxRegisters.TabStop = false;
+            this.groupBoxRegisters.Text = "Registers";
+            // 
+            // textBoxRegisterA
+            // 
+            this.textBoxRegisterA.Location = new System.Drawing.Point(81, 29);
+            this.textBoxRegisterA.Name = "textBoxRegisterA";
+            this.textBoxRegisterA.Size = new System.Drawing.Size(50, 22);
+            this.textBoxRegisterA.TabIndex = 1;
+            // 
+            // textBoxRegisterB
+            // 
+            this.textBoxRegisterB.Location = new System.Drawing.Point(81, 56);
+            this.textBoxRegisterB.Name = "textBoxRegisterB";
+            this.textBoxRegisterB.Size = new System.Drawing.Size(50, 22);
+            this.textBoxRegisterB.TabIndex = 9;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(30, 59);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(17, 17);
+            this.label12.TabIndex = 8;
+            this.label12.Text = "B";
+            // 
+            // textBoxRegisterD
+            // 
+            this.textBoxRegisterD.Location = new System.Drawing.Point(81, 111);
+            this.textBoxRegisterD.Name = "textBoxRegisterD";
+            this.textBoxRegisterD.Size = new System.Drawing.Size(50, 22);
+            this.textBoxRegisterD.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "D";
+            // 
+            // textBoxRegisterC
+            // 
+            this.textBoxRegisterC.Location = new System.Drawing.Point(81, 84);
+            this.textBoxRegisterC.Name = "textBoxRegisterC";
+            this.textBoxRegisterC.Size = new System.Drawing.Size(50, 22);
+            this.textBoxRegisterC.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 87);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(17, 17);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "C";
+            // 
+            // textBoxRegisterF
+            // 
+            this.textBoxRegisterF.Location = new System.Drawing.Point(81, 166);
+            this.textBoxRegisterF.Name = "textBoxRegisterF";
+            this.textBoxRegisterF.Size = new System.Drawing.Size(50, 22);
+            this.textBoxRegisterF.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(30, 169);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(16, 17);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "F";
+            // 
+            // textBoxRegisterE
+            // 
+            this.textBoxRegisterE.Location = new System.Drawing.Point(81, 139);
+            this.textBoxRegisterE.Name = "textBoxRegisterE";
+            this.textBoxRegisterE.Size = new System.Drawing.Size(50, 22);
+            this.textBoxRegisterE.TabIndex = 15;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(30, 142);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(17, 17);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "E";
+            // 
+            // textBoxRegisterL
+            // 
+            this.textBoxRegisterL.Location = new System.Drawing.Point(81, 220);
+            this.textBoxRegisterL.Name = "textBoxRegisterL";
+            this.textBoxRegisterL.Size = new System.Drawing.Size(50, 22);
+            this.textBoxRegisterL.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "L";
+            // 
+            // textBoxRegisterH
+            // 
+            this.textBoxRegisterH.Location = new System.Drawing.Point(81, 193);
+            this.textBoxRegisterH.Name = "textBoxRegisterH";
+            this.textBoxRegisterH.Size = new System.Drawing.Size(50, 22);
+            this.textBoxRegisterH.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "H";
+            // 
+            // buttonStartEmulation
+            // 
+            this.buttonStartEmulation.Location = new System.Drawing.Point(503, 313);
+            this.buttonStartEmulation.Name = "buttonStartEmulation";
+            this.buttonStartEmulation.Size = new System.Drawing.Size(75, 34);
+            this.buttonStartEmulation.TabIndex = 10;
+            this.buttonStartEmulation.Text = "Start";
+            this.buttonStartEmulation.UseVisualStyleBackColor = true;
+            // 
+            // buttonPauseEmulation
+            // 
+            this.buttonPauseEmulation.Location = new System.Drawing.Point(503, 353);
+            this.buttonPauseEmulation.Name = "buttonPauseEmulation";
+            this.buttonPauseEmulation.Size = new System.Drawing.Size(75, 49);
+            this.buttonPauseEmulation.TabIndex = 11;
+            this.buttonPauseEmulation.Text = "Pause";
+            this.buttonPauseEmulation.UseVisualStyleBackColor = true;
+            // 
+            // buttonStopEmulation
+            // 
+            this.buttonStopEmulation.Location = new System.Drawing.Point(503, 408);
+            this.buttonStopEmulation.Name = "buttonStopEmulation";
+            this.buttonStopEmulation.Size = new System.Drawing.Size(75, 39);
+            this.buttonStopEmulation.TabIndex = 12;
+            this.buttonStopEmulation.Text = "Stop";
+            this.buttonStopEmulation.UseVisualStyleBackColor = true;
+            // 
+            // textBoxMessages
+            // 
+            this.textBoxMessages.Location = new System.Drawing.Point(28, 309);
+            this.textBoxMessages.Multiline = true;
+            this.textBoxMessages.Name = "textBoxMessages";
+            this.textBoxMessages.Size = new System.Drawing.Size(436, 137);
+            this.textBoxMessages.TabIndex = 13;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(675, 470);
+            this.Controls.Add(this.textBoxMessages);
+            this.Controls.Add(this.buttonStopEmulation);
+            this.Controls.Add(this.buttonPauseEmulation);
+            this.Controls.Add(this.buttonStartEmulation);
+            this.Controls.Add(this.groupBoxRegisters);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
             this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.groupBoxRegisters.ResumeLayout(false);
+            this.groupBoxRegisters.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelRegisterA;
+        private System.Windows.Forms.GroupBox groupBoxRegisters;
+        private System.Windows.Forms.TextBox textBoxRegisterA;
+        private System.Windows.Forms.TextBox textBoxRegisterL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxRegisterH;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxRegisterF;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBoxRegisterE;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBoxRegisterD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxRegisterC;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxRegisterB;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button buttonStartEmulation;
+        private System.Windows.Forms.Button buttonPauseEmulation;
+        private System.Windows.Forms.Button buttonStopEmulation;
+        private System.Windows.Forms.TextBox textBoxMessages;
     }
 }
--- JameBoyV1/MainWindow.cs
+++ JameBoyV1/MainWindow.cs
@@ -21,5 +21,6 @@ namespace JameBoyV1
         {
 
         }
+
     }
 }
